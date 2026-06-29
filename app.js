6bebb0689c4effc3791527cab3920cfa12622c6d
Hasan <yigitankuva@gmail.com>
2026-06-29 17:43:56 +0300

    fix: trust proxy eklendi, Railway HTTPS arkasinda secure cookie calismiyor

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const { requireFirma } = require('./middleware/authMiddleware');
 
 const app = express();
 
+app.set('trust proxy', 1);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(ejsLayouts);
