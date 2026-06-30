d5e2613e0db4a7f57c112b18f3321e95b81c2a95
Hasan Yigit <yigitankuva@gmail.com>
2026-06-30 09:45:24 +0300

    feat: redirect root / to /firma/giris

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.use('/firma', authRoutes);
 app.use('/firma/panel', requireFirma, panelRoutes);
 app.use('/superadmin', superadminRoutes);
 app.use('/bayi', bayiRoutes);
+app.get('/', (req, res) => res.redirect('/firma/giris'));
 app.use('/', publicRoutes);
 
 const PORT = process.env.PORT || 3000;
